79249566576d0d3f434517d999bea5ea14dfa5c4
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2023-05-26 01:20:17 +0530

    pokedex list screen done

---
+++ lib/src/config/colors.dart
@@ -0,0 +1,77 @@
+
+import 'package:flutter/material.dart';
+
+const Map<String, Map<String, Color>> typeColors = {
+  "Electric": {
+    "background": Color.fromRGBO(255, 255, 0, 0.5),
+    "borderColor": Color.fromRGBO(255, 255, 0, 1)
+  },
+  "Water": {
+    "background": Color.fromRGBO(0, 0, 255, 0.5),
+    "borderColor": Color.fromRGBO(0, 0, 255, 1)
+  },
+  "Psychic": {
+    "background": Color.fromRGBO(128, 0, 128, 0.5),
+    "borderColor": Color.fromRGBO(128, 0, 128, 1)
+  },
+  "Bug": {
+    "background": Color.fromRGBO(172, 255, 47, 0.5),
+    "borderColor": Color.fromRGBO(173, 255, 47, 1)
+  },
+  "Poison": {
+    "background": Color.fromRGBO(238, 130, 238, 0.5),
+    "borderColor": Color.fromRGBO(238, 130, 238, 1)
+  },
+  "Ghost": {
+    "background": Color.fromRGBO(210, 59, 210, 0.5),
+    "borderColor": Color.fromRGBO(210, 59, 210, 1)
+  },
+  "Fire": {
+    "background": Color.fromRGBO(255, 0, 0, 0.5),
+    "borderColor": Color.fromRGBO(255, 0, 0, 1)
+  },
+  "Fairy": {
+    "background": Color.fromRGBO(255, 192, 203, 0.5),
+    "borderColor": Color.fromRGBO(255, 12, 203, 1)
+  },
+  "Ground": {
+    "background": Color.fromRGBO(140, 90, 55, 0.5),
+    "borderColor": Color.fromRGBO(140, 90, 55, 1)
+  },
+  "Ice": {
+    "background": Color.fromRGBO(127, 255, 212, 0.5),
+    "borderColor": Color.fromRGBO(127, 255, 212, 1)
+  },
+  "Dragon": {
+    "background": Color.fromRGBO(102, 0, 102, 0.5),
+    "borderColor": Color.fromRGBO(128,0,128,1)
+  },
+  "Grass": {
+    "background": Color.fromRGBO(144, 238, 144, 0.5),
+    "borderColor":Color.fromRGBO(0,128,0,1)
+  },
+  "Flying": {
+    "background": Color.fromRGBO(70, 131, 180, 0.5),
+    "borderColor":Color.fromRGBO(69, 131, 181, 1)
+  },
+  "Normal": {
+    "background": Color.fromRGBO(222, 184, 135, 0.5),
+    "borderColor": Color.fromRGBO(222, 184, 135, 1)
+  },
+  "Rock": {
+    "background": Color.fromRGBO(128, 128, 128, 0.5),
+    "borderColor": Color.fromRGBO(128, 128, 128, 1)
+  },
+  "Dark": {
+    "background": Color.fromRGBO(62, 62, 62, 0.5),
+    "borderColor": Color.fromRGBO(62, 62, 62, 1)
+  },
+  "Steel": {
+    "background": Color.fromRGBO(169, 169, 169, 0.5),
+    "borderColor": Color.fromRGBO(169, 169, 169, 1)
+  },
+  "Fighting": {
+    "background": Color.fromRGBO(160, 10, 10, 0.5),
+    "borderColor":Color.fromRGBO(160, 10, 10, 1)
+  },
+};
