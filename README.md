270f32abb46b7e7935104a77451c3a0ca43b0829
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2023-06-06 00:38:52 +0530

    readme update

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # Pokedex App
+- New addition to my [Pokedex Project](https://github.com/RevTpark/pokeDex), A pokedex app made using Flutter.
+- Integrated Tflite model for Image Classification.
+- Smooth Animations and User Interfance/User Experience
+- Custom Icons and Vectors made by me
+
 ## App Demo Video
 <div align='center'>
   <video src="https://github.com/RevTpark/pokeDex-app/assets/78678620/3b78b44e-d442-4ad0-ba3c-b811b45c6dbd" />
 </div>
-<div style='display:flex; flex-direction: row;'>
+
+## App Screenshots
+<div align='center'>
+    <h3>Menu</h3>
   <div>
     <img src="https://github.com/RevTpark/pokeDex-app/assets/78678620/13561fee-134e-49df-a3a6-3acdc8e17a13" height='500px'></img>
   </div>
+    <h3>Image Classification</h3>
   <div>
     <img src="https://github.com/RevTpark/pokeDex-app/assets/78678620/ba3b114a-13ef-458e-9fba-892792021192" height='500px'></img>  
   </div>
+    <h3>Pokedex</h3>
   <div>
     <img src="https://github.com/RevTpark/pokeDex-app/assets/78678620/0bb62863-08d6-46fd-bb16-635e487fd4be" height='500px'></img>
   </div>
--- lib/src/widgets/rate_my_team.dart
+++ lib/src/widgets/rate_my_team.dart
@@ -7,7 +7,6 @@ import 'package:pokedex_mobile_app/src/api/pokemon_service.dart';
 import 'package:pokedex_mobile_app/src/models/pokemon.dart';
 import 'package:provider/provider.dart';
 
-
 class RateMyTeamDisplay extends StatefulWidget {
   const RateMyTeamDisplay({Key? key}) : super(key: key);
 
@@ -18,6 +17,21 @@ class RateMyTeamDisplay extends StatefulWidget {
 class _RateMyTeamDisplayState extends State<RateMyTeamDisplay> {
   int _currentIndex = 0;
   String _currentImageUrl = "";
+  late final Map<String, dynamic> generationsList;
+  String currentGen = '1-151';
+
+  @override
+  void initState() {
+    super.initState();
+    initGenList();
+  }
+
+  void initGenList() async {
+    final generations = await PokemonService().getGenerations();
+    setState(() {
+      generationsList = generations;
+    });
+  }
 
   Future<List<Pokemon>> getPokemonList(RateTeamProvider provider) async {
     final List<Pokemon> pokemonList = await PokemonService().getAllPokemon();
@@ -42,14 +56,17 @@ class _RateMyTeamDisplayState extends State<RateMyTeamDisplay> {
                   margin: EdgeInsets.all(10),
                   width: 75,
                   decoration: BoxDecoration(
-                      color: _currentIndex == idx ? Colors.lightBlue.shade100 : Colors.white24,
-                    border: Border.all(
-                      color: _currentIndex == idx ? Colors.lightBlue : Colors.white,
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(10))
-                  ),
-                  child: Image.network(
-                      item.image,
+                      color: _currentIndex == idx
+                          ? Colors.lightBlue.shade100
+                          : Colors.white24,
+                      border: Border.all(
+                        color: _currentIndex == idx
+                            ? Colors.lightBlue
+                            : Colors.white,
+                      ),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                  child: Image.network(item.image,
                       errorBuilder: (context, exception, stackTrace) {
                     return Image.asset(
                       "assets/images/pokeball_loader.png",
@@ -57,14 +74,18 @@ class _RateMyTeamDisplayState extends State<RateMyTeamDisplay> {
                   })),
             ),
           );
-        }).values.toList();
+        })
+        .values
+        .toList();
   }
 
-  List<Widget> makePokemonListView(data){
-    return data.map<Widget>((Pokemon item) => Container(
-      color: Colors.blueGrey,
-      child: Text(item.name),
-    )).toList();
+  List<Widget> makePokemonListView(data) {
+    return data
+        .map<Widget>((Pokemon item) => Container(
+              color: Colors.blueGrey,
+              child: Text(item.name),
+            ))
+        .toList();
   }
 
   @override
@@ -101,61 +122,96 @@ class _RateMyTeamDisplayState extends State<RateMyTeamDisplay> {
                         ),
                       ),
                       Container(
-                        height: height * 0.35.w,
-                        // width: width.w,
-                        // color: Colors.black,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            SizedBox(
-                              width: width*0.45.w,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Image.network(
-                                      _currentImageUrl,
-                                      errorBuilder: (context, exception, stackTrace) {
-                                      return Image.asset(
-                                        "assets/images/pokeball_loader.png",
-                                      );
-                                    }
-                                  ),
-                                  Container(
-                                      child: Text("Select Pokemon", style: TextStyle(color: Colors.white),)
-                                  )
-                                ],
+                          height: height * 0.35.w,
+                          // width: width.w,
+                          // color: Colors.black,
+                          child: Row(
+                            children: [
+                              // SizedBox(
+                              //   width: width*0.45.w,
+                              //   child: Column(
+                              //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              //     children: [
+                              //       Image.network(
+                              //           _currentImageUrl,
+                              //           errorBuilder: (context, exception, stackTrace) {
+                              //           return Image.asset(
+                              //             "assets/images/pokeball_loader.png",
+                              //           );
+                              //         }
+                              //       ),
+                              //       Container(
+                              //           child: Text("Select Pokemon", style: TextStyle(color: Colors.white),)
+                              //       )
+                              //     ],
+                              //   ),
+                              // ),
+                              SizedBox(
+                                width: width * 0.45.w,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    DropdownButton(
+                                        elevation: 16,
+                                        underline: Container(
+                                          height: 2,
+                                          color: Colors.deepPurpleAccent,
+                                        ),
+                                        value: currentGen,
+                                        items: generationsList.entries
+                                            .map<DropdownMenuItem<String>>((entry) {
+                                              return DropdownMenuItem<String>(
+                                                value: entry.value,
+                                                child: Text(entry.key),
+                                                );
+                                              }).toList(),
+                                        onChanged: (String? value) {
+                                          setState(() {
+                                            currentGen = value!;
+                                          });
+                                        })
+                                  ],
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              width: width * 0.5.w,
-                              child: ListWheelScrollView(
-                                  physics: BouncingScrollPhysics(),
-                                  onSelectedItemChanged: (item) => {
-                                    setState(() {
-                                      _currentImageUrl = snapshot.data![item].image;
-                                    })
-                                  },
-                                  itemExtent: 50,
-                                  children: makePokemonListView(snapshot.data)
-                              ),
-                            )
-                          ],
-                        )
-                      )
+                              SizedBox(
+                                width: width * 0.5.w,
+                                child: ListWheelScrollView(
+                                    physics: BouncingScrollPhysics(),
+                                    onSelectedItemChanged: (item) => {
+                                          setState(() {
+                                            _currentImageUrl =
+                                                snapshot.data![item].image;
+                                          })
+                                        },
+                                    itemExtent: 50,
+                                    children:
+                                        makePokemonListView(snapshot.data)),
+                              )
+                            ],
+                          ))
                     ],
                   ),
                 );
               } else {
                 return Container(
                     padding: const EdgeInsets.all(10.0),
-                    child: const CircularProgressIndicator());
+                    height: height * 0.5.w,
+                    child: const Center(child: CircularProgressIndicator()));
               }
             }),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const[
-            MenuTextBox(title: "Home", changeTo: ScreenLayout.home, widthFactor: 0.45,),
-            MenuTextBox(title: "Get Ranked", changeTo: ScreenLayout.ratedTeamDisplay, widthFactor: 0.45,)
+          children: const [
+            MenuTextBox(
+              title: "Home",
+              changeTo: ScreenLayout.home,
+              widthFactor: 0.45,
+            ),
+            MenuTextBox(
+              title: "Get Ranked",
+              changeTo: ScreenLayout.ratedTeamDisplay,
+              widthFactor: 0.45,
+            )
           ],
         )
       ],
